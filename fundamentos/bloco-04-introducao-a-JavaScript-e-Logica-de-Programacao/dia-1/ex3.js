7ac082f687cb8ef9d21365e4e57ff5549867a5ba
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-07-28 19:55:54 -0300

    exercicio 3

---
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-1/ex3.js
@@ -0,0 +1,11 @@
+let num1 = 15;
+let num2 = 8;
+let num3 = 23;
+
+if (num1 > num2 && num1 > num3){
+  console.log("O maior é", num1);
+} else if (num2 > num3){
+  console.log("O maior é", num2);
+} else {
+  console.log("O maior é", num3);
+}
