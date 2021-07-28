153baca9cc3dcdcdcb50a55473ef54174c449f7c
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-07-28 20:24:18 -0300

    exercicio 7

---
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-1/ex7.js
@@ -0,0 +1,18 @@
+let nota = 110;
+
+if (nota < 0 || nota >= 100){
+  console.log("Nota inválida.");
+} else if (nota >= 90){
+  console.log("A");
+} else if (nota >= 80){
+  console.log("B");
+} else if (nota >= 70){
+  console.log("C");
+} else if (nota >= 60){
+  console.log("D");
+} else if (nota >= 50){
+  console.log("E");
+} else {
+  console.log("F");
+}
+
