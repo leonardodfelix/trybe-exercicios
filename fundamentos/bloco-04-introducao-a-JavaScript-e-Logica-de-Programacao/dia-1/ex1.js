2c69ea68b707d9abd5acaea5cbdc4a9dcf5bf971
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-07-28 19:11:09 -0300

    exercicio 1

---
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-1/ex1.js
@@ -0,0 +1,28 @@
+let num1 = 10;
+let num2 = 5;
+
+function addNumbers(x1, x2){
+  return console.log(x1 + x2);
+}
+
+function subtNumbers(x1, x2) {
+  return console.log(x1 - x2);  
+}
+
+function multNumbers(x1, x2) {
+  return console.log(x1 * x2);  
+}
+
+function divNumbers(x1, x2) {
+  return console.log(x1 / x2)
+}
+
+function modNumbers(x1, x2){
+  return console.log(x1 % x2)
+}
+
+addNumbers(num1, num2);
+subtNumbers(num1, num2);
+multNumbers(num1, num2);
+divNumbers(num1, num2);
+modNumbers(num1, num2);
