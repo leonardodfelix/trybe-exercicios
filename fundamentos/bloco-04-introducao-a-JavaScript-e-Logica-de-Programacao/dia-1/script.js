7741e0c941c978f72746e332a638845409ce3250
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-07-28 17:13:09 -0300

    fix script.js

--- fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-1/script.js
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-1/script.js
@@ -112,22 +112,24 @@
 // Crie uma variável para armazenar o estado da pessoa candidata no processo seletivo, que pode ser: 'aprovada' , 'lista' ou 'reprovada' ;
 // Crie uma estrutura condicional com o switch/case que irá imprimir as mensagens do exercício anterior se o estado da pessoa candidata for 'aprovada' , 'lista' ou 'reprovada' . Como default , imprima a mensagem de 'não se aplica' .
 
-let candidateState = "lista";
+// let candidateState = "lista";
 
-switch (candidateState) {
-  case "aprovada":
-    console.log("Parabéns, você foi aprovada(o)!");
-    break;
+// switch (candidateState) {
+//   case "aprovada":
+//     console.log("Parabéns, você foi aprovada(o)!");
+//     break;
 
-  case "lista":
-    console.log("Você está na nossa lista de espera");
-    break;
+//   case "lista":
+//     console.log("Você está na nossa lista de espera");
+//     break;
 
-  case "reprovada":
-    console.log("Você foi reprovada(o)")
-    break;
+//   case "reprovada":
+//     console.log("Você foi reprovada(o)")
+//     break;
 
-  default:
-    console.log("Estado não identificado.")
-    break;
-}
+//   default:
+//     console.log("Estado não identificado.")
+//     break;
+// }
+
+// #endregion
