bafe9103f515e97a227deea2ee55216f94297d99
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-07-29 11:01:45 -0300

    Exercicios bonus

---
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-2/bonus.js
@@ -0,0 +1,27 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let crescNumbers = numbers.slice();
+let decrescNumbers = [...numbers]; // Higher performance - ... spread operator
+
+for (let index = 1; index < crescNumbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (crescNumbers[index] < crescNumbers[secondIndex]) {
+      let position = crescNumbers[index];
+      crescNumbers[index] = crescNumbers[secondIndex];
+      crescNumbers[secondIndex] = position;
+    }
+  }
+}
+
+for (let index = 1; index < decrescNumbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (decrescNumbers[index] > decrescNumbers[secondIndex]) {
+      let position = decrescNumbers[index];
+      decrescNumbers[index] = decrescNumbers[secondIndex];
+      decrescNumbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers);
+console.log(crescNumbers);
+console.log(decrescNumbers);
