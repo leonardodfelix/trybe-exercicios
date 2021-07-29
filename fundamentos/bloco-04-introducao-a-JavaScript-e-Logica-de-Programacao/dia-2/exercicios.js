3f1807285c90282930615c9562a5c97e263933e6
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-07-29 09:00:47 -0300

    Exercicio 6

--- fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-2/exercicios.js
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-2/exercicios.js
@@ -11,7 +11,7 @@ for (let number of numbers) {
 let sum = 0;
 
 for (let number of numbers) {
-  sum+= number;
+  sum += number;
 }
 
 console.log("A soma é:", sum);
@@ -24,7 +24,7 @@ console.log("O valor médio do array é:", mean.toFixed(2));
 
 // Ex 4
 
-if (mean > 20){
+if (mean > 20) {
   console.log('valor maior que 20');
 } else {
   console.log('valor menor ou igual a 20');
@@ -34,11 +34,28 @@ if (mean > 20){
 
 let maior = numbers[0];
 
-for (let index = 0; index < numbers.length; index+= 1) {
-  if (numbers[index] > maior){
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > maior) {
     maior = numbers[index];
   }
 }
 
 console.log("O maior é", maior);
 
+// Ex 6
+
+let qtdeImpar = 0;
+
+for (let number of numbers) {
+  if ((number % 2) == 1) {
+    qtdeImpar += 1;
+  }
+}
+
+if (qtdeImpar) {
+  console.log("A quantidade de números ímpares é", qtdeImpar);
+} else {
+  console.log("nenhum valor ímpar encontrado");
+}
+
+// Ex 7
