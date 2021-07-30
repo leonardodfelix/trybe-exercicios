d5349cf8293be9f75581a6c15be48400b7a318b2
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-07-30 10:04:48 -0300

    Exercicio 4

--- fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-3/script.js
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-3/script.js
@@ -1,20 +1,31 @@
+let n = 5;
+let spaces = 0;
+
 // Ex.1
 
-let n = 5;
-
-for (let index1 = 0; index1 < n; index1 += 1){
-  console.log('*'.repeat(n));
+for (let index = 0; index < n; index += 1) {
+  spaces = n;
+  console.log('*'.repeat(spaces));
 }
- 
+
 // Ex.2
 
-for (let index2 = 0; index2 < n; index2 += 1) {
-  console.log('*'.repeat(index2));
+for (let index = 0; index < n; index += 1) {
+  console.log('*'.repeat(index));
 }
 
 // Ex.3
 
-for (let index2 = 0; index2 < n; index2 += 1) {
-  console.log(' '.repeat(n-index2)+'*'.repeat(index2));
+for (let index = 0; index < n; index += 1) {
+  spaces = n - index;
+  console.log(' '.repeat(spaces) + '*'.repeat(index));
 }
 
+// Ex.4
+
+for (let index = 0; index < n; index += 2) {
+  spaces = Math.floor((n - index)/2);
+  console.log(' '.repeat(spaces)+
+              '*'.repeat(n - (2*spaces))+
+              ' '.repeat(spaces));
+}
