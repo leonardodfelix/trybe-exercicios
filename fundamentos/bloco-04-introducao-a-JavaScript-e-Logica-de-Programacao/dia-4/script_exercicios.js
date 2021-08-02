7eb4920fd3e40bdc06cf921e8854ef2f97270010
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-02 09:42:05 -0300

    Exercicios 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-4/script_exercicios.js
@@ -0,0 +1,17 @@
+// Parte I
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// Ex.1
+
+console.log('Bem-vinda, ' + info.personagem);
+
+// Ex.2
+
+info['recorrente'] = 'Sim';
+
+console.log(info);
