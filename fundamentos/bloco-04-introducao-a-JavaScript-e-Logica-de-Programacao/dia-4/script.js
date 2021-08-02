37b504463b0b5c20bedc97ae7628c8afc9904026
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-02 09:00:45 -0300

    Conteudo - ex. 5

--- fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-4/script.js
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-4/script.js
@@ -17,5 +17,11 @@ console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + play
 
 player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 
+// Ex.4
+
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes');
+
+// Ex.5
+
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
 
