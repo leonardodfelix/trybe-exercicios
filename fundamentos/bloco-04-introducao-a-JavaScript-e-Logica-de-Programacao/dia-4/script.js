85a6d6b04abef614a023349a0601f54bc9540aee
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-02 08:55:02 -0300

    Conteudo - ex. 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-4/script.js
@@ -0,0 +1,17 @@
+// Conteúdo
+// Objetos
+// Ex.1
+
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: { golden: 2, silver: 3 }
+}
+
+// Ex.2
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
+
+// Ex.3
+
