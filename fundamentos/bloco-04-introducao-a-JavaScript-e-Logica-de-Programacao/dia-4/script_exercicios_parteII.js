b2d8b40b08ebbe02ddabb0fc7e38e7c1ca700f40
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-02 14:37:04 -0300

    Exercicio 5 parte II

--- fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-4/script_exercicios_parteII.js
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Logica-de-Programacao/dia-4/script_exercicios_parteII.js
@@ -1,10 +1,10 @@
 // Parte II
 // Ex.1
 
-function verificaPalindromo(palavra){
+function verificaPalindromo(palavra) {
   let arvalap = palavra.split("").reverse().join("");
 
-  if (palavra === arvalap){
+  if (palavra === arvalap) {
     console.log(true);
   } else {
     console.log(false);
@@ -22,7 +22,7 @@ function maiorValor(array) {
   let maior = array[0];
   let index = 0;
   for (let num in array) {
-    if (array[num] > maior){
+    if (array[num] > maior) {
       maior = array[num];
       index = num;
     }
@@ -40,7 +40,7 @@ function menorValor(array) {
   let menor = array[0];
   let index = 0;
   for (let num in array) {
-    if (array[num] < menor){
+    if (array[num] < menor) {
       menor = array[num];
       index = num;
     }
@@ -54,10 +54,10 @@ console.log(menorValor(array2));
 
 let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
-function maiorNome(array){
+function maiorNome(array) {
   let maiorString = array[0];
   for (let index in array) {
-    if (array[index].length > maiorString.length){
+    if (array[index].length > maiorString.length) {
       maiorString = array[index];
     }
   }
@@ -67,3 +67,28 @@ function maiorNome(array){
 console.log(maiorNome(nomes));
 
 // Ex.5
+
+let numbers = [9, 2, 3, 2, 5, 8, 2, 3];
+
+function maisRepete(array) {
+  let maisNum = array[0];
+  let ocorrencias = 0;
+  for (let number of array) {
+    let numCount = 0;
+    for (let compareNumber of array) {
+      if (compareNumber === number) {
+        numCount += 1;
+      }
+    }
+    if (numCount > ocorrencias) {
+      ocorrencias = numCount;
+      maisNum = number;
+    }
+  }
+  return maisNum;
+}
+
+console.log(maisRepete(numbers));
+
+// Ex.6
+
