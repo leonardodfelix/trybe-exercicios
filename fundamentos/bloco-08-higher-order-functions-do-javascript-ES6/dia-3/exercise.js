a55fe41d3085b7e240954ecda842e6e0b966dda0
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-09-08 16:56:25 -0300

    Exercise 3 done

--- fundamentos/bloco-08-higher-order-functions-do-javascript-ES6/dia-3/exercise.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-ES6/dia-3/exercise.js
@@ -124,3 +124,45 @@ assert.deepStrictEqual(nameAndAge(), expectedResult2);
 
 //#endregion
 
+// #region 3 de 7
+const expectedResult = [
+  { 
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965
+  }
+];
+
+function fantasyOrScienceFiction() {
+  return books.filter( book => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
+}
+
+assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
+//#endregion
+
+// #region 4 de 7
+
+// #endregion
