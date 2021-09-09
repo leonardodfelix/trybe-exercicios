a5030007e804df84aa65a4aad0941ec8a3cad60d
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-09-09 13:22:49 -0300

    Exercise 1 done

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-ES6/dia-4/exercises.js
@@ -0,0 +1,16 @@
+//#region 1 of 6
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acc, curr) => acc.concat(curr));
+}
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+//#endregion
+
