3e061fa5129de1dc8de167a7ef704a1af9543b59
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-10-04 15:39:54 -0300

    Exercise 3 done

--- desenvolvimento front-end/bloco-11-introducao-a-react/dia-1/exercise/src/App.js
+++ desenvolvimento front-end/bloco-11-introducao-a-react/dia-1/exercise/src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
-const taskList = ['Lavar a louça', 'Fazer compras', 'Fornicar'];
+// const taskList = ['Lavar a louça', 'Fazer compras', 'Fornicar'];
 
-const Task = (value) => {
+// const Task = (value) => {
+//   return (
+//     <li>{value}</li>
+//   );
+// }
+
+// function App() {
+//   return taskList.map(task => Task(task));
+// }
+
+const App = () => {
   return (
-    <li>{value}</li>
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   );
-}
-
-function App() {
-  return taskList.map(task => Task(task));
 }
 
 export default App;
