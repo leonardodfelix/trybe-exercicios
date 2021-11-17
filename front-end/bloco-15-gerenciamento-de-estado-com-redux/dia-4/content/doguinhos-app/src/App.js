622e812e2fc56a8c01c332ab030d85ff4b0986cb
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-11-17 09:29:53 -0300

    Content done

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4/content/doguinhos-app/src/App.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4/content/doguinhos-app/src/App.js
@@ -1,8 +1,30 @@
-function App() {
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchDog } from './store';
+
+function App({ isFetching, src, fetchDog2 }) {
   return (
-    <div>
-    </div>
+    isFetching ? <p>Loading</p>
+      : (
+        <div style={{ width: 500 }}>
+          <button
+            style={{ display: 'block' }}
+            onClick={fetchDog2}
+            type="button"
+          >
+            Novo Doguinho
+          </button>
+          <img style={{ width: '100%' }} src={src} alt="dog" />
+        </div>
+      )
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  src: state.imagePath,
+  isFetching: state.isFetching});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchDog2: () => dispatch(fetchDog())});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
